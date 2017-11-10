301bbc4eebd268b630cb265dfc0b07dc94c69520
Tim <>
2017-11-10 13:01:38 +0300

    new commit

---
+++ UsingUnitTests/UsingUnitTests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UsingUnitTests
+//
+//  Created by user on 09.11.2017.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UsingUnitTests/UsingUnitTests/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UsingUnitTests
+//
+//  Created by user on 09.11.2017.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
